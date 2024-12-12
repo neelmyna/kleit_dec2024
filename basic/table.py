3ecfe8de06842a0efc32242ef169bb2388cc3258
neelmyna <nithincheeta@gmail.com>
2024-12-12 11:33:16 +0530

    shape X program added

--- basic/table.py
+++ basic/table.py
@@ -1,10 +1,12 @@
 # cline1.py
 import sys
 
-# python table.py 14
-
 input_number = int(sys.argv[1])
 
-for i in range(1, 21):
-    #print(f'{input_number} * {i} = {input_number * i}')
-    print('%d * %02d = %03d'%(input_number, i, input_number*i))
+for i in range(1, input_number+1):
+    for j in range(1, input_number+1):
+        if i == j or j == input_number - i + 1:
+            print('* ', end='')
+        else:
+            print('  ', end='')
+    print()
